4b90300439bf97a58b2767bce0f3d628a3bc87c1
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-24 09:41:26 +0300

    change Base url

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import { App } from 'components/App';
 import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
 import { ThemeStatus } from 'components/ThemeStatus/ThemeProvider';
 
-window.global = {
-  BASE_URL: 'http://localhost:3030/api',
-  BASE_URL_IMG: 'http://localhost:3030/uploads/',
-};
 // window.global = {
-//   BASE_URL: 'https://salamandrabackend.studentvlad5.repl.co/api',
-//   BASE_URL_IMG: 'https://salamandrabackend.studentvlad5.repl.co/uploads/',
+//   BASE_URL: 'http://localhost:3030/api',
+//   BASE_URL_IMG: 'http://localhost:3030/uploads/',
 // };
+window.global = {
+  BASE_URL: 'https://salamandrabackend.studentvlad5.repl.co/api',
+  BASE_URL_IMG: 'https://salamandrabackend.studentvlad5.repl.co/uploads/',
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
