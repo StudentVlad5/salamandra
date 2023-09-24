4b0915ecb1bd6b018c8a5de7c89a630a1a33bedd
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-24 04:13:21 +0000

    Deploying to gh-pages from @ StudentVlad5/salamandra@558e4a6723ffb8d32f8877646e6cca871b5c4a3f 🚀

--- src/redux/modal/operation.js
+++
@@ -1,8 +0,0 @@
-import { modalSlice } from './slice';
-
-export const addModal = b => dispatch => {
-  dispatch(modalSlice.actions.addModal({ ...b }));
-};
-export const cleanModal = () => dispatch => {
-  dispatch(modalSlice.actions.cleanModal());
-};
--- src/redux/reload/selectors.js
+++
@@ -1,1 +0,0 @@
-export const reloadValue = (state) => state.reload.value;
